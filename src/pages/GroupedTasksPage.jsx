7b8550ca486bc33d84075115f4527a88d4cca857
HarshMaurya25 <mauryaharsh2505@gmail.com>
2026-04-27 12:05:55 +0530

    Bug fix : Useemo

--- src/pages/GroupedTasksPage.jsx
+++ src/pages/GroupedTasksPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import {
   Plus, Folder, ChevronRight, RotateCcw, Star, Palette, Trash2, 
